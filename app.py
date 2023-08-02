01b2f5e8a016b752faef4bd67e38ac9c02cf6f4b
ianoxplix <65483660+ianoxplix@users.noreply.github.com>
2023-08-02 18:28:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def post_products():
         try:
             cursor.execute("INSERT INTO items(ProductID,ProductName,NewPrice,Category,date,oldprice,PICTURE_url,spare) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)", (ProductID, ProductName, NewPrice, Category, date, oldprice, PICTURE_url, spare))
             connection.commit()
-     else:
+         else:
         cursor.execute("SELECT ProductID COUNT(*) FROM items HAVING COUNT(*)>1")connection.comimit.()
         except:
             return render_template('register.html', msg1="Unable to create another Entty with {}".format(ProductID))
